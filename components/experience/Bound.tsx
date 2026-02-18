a61b7dd2b6ff96b910e9de00ead44eb3859c8eb1
jungminyoo <yoojm4718@gmail.com>
2026-02-19 00:12:56 +0900

    feat : added bounds

--- components/experience/Bound.tsx
+++ components/experience/Bound.tsx
@@ -1,4 +1,9 @@
-import { FLOOR_SCALE } from "@/resources/constants";
+import {
+  BOUND_HEIGHT,
+  BOUND_HEIGHT_OFFSET,
+  BOUND_THICKNESS,
+  FLOOR_SCALE,
+} from "@/resources/constants";
 import { CuboidCollider, RigidBody } from "@react-three/rapier";
 
 export default function Bound() {
@@ -6,8 +11,40 @@ export default function Bound() {
     <>
       <RigidBody type="fixed" restitution={0.1} friction={0.5}>
         <CuboidCollider
-          args={[FLOOR_SCALE[0] / 2, 1, FLOOR_SCALE[1] / 2]}
-          position={[0, -1, 0]}
+          args={[FLOOR_SCALE[0] / 2, BOUND_THICKNESS / 2, FLOOR_SCALE[1] / 2]}
+          position={[0, -BOUND_THICKNESS / 2, 0]}
+        />
+        <CuboidCollider
+          args={[FLOOR_SCALE[0] / 2, BOUND_HEIGHT / 2, BOUND_THICKNESS / 2]}
+          position={[
+            0,
+            BOUND_HEIGHT / 2 - BOUND_HEIGHT_OFFSET,
+            -(FLOOR_SCALE[1] / 2 + BOUND_THICKNESS / 2),
+          ]}
+        />
+        <CuboidCollider
+          args={[FLOOR_SCALE[0] / 2, BOUND_HEIGHT / 2, BOUND_THICKNESS / 2]}
+          position={[
+            0,
+            BOUND_HEIGHT / 2 - BOUND_HEIGHT_OFFSET,
+            FLOOR_SCALE[1] / 2 + BOUND_THICKNESS / 2,
+          ]}
+        />
+        <CuboidCollider
+          args={[BOUND_THICKNESS / 2, BOUND_HEIGHT / 2, FLOOR_SCALE[1] / 2]}
+          position={[
+            -(FLOOR_SCALE[0] / 2 + BOUND_THICKNESS / 2),
+            BOUND_HEIGHT / 2 - BOUND_HEIGHT_OFFSET,
+            0,
+          ]}
+        />
+        <CuboidCollider
+          args={[BOUND_THICKNESS / 2, BOUND_HEIGHT / 2, FLOOR_SCALE[1] / 2]}
+          position={[
+            FLOOR_SCALE[0] / 2 + BOUND_THICKNESS / 2,
+            BOUND_HEIGHT / 2 - BOUND_HEIGHT_OFFSET,
+            0,
+          ]}
         />
       </RigidBody>
     </>
--- resources/constants.ts
+++ resources/constants.ts
@@ -1,3 +1,6 @@
 export const FLOOR_SCALE: [x: number, y: number, z: number] = [42, 18, 1];
 export const START_HEIGHT = 16;
 export const START_CAMERA_HEIGHT = 40;
+export const BOUND_THICKNESS = 2;
+export const BOUND_HEIGHT = 6;
+export const BOUND_HEIGHT_OFFSET = 0.5;
